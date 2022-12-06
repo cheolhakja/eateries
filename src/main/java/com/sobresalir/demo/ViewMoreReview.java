164053edb01d6c62796922d0f651abc0a2d0bc97
jeon-ji-yoon <rebegin15@gmail.com>
2022-12-06 11:36:57 +0900

    add: scrap review data

--- src/main/java/com/sobresalir/demo/ViewMoreReview.java
+++ src/main/java/com/sobresalir/demo/ViewMoreReview.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.security.Key;
+import java.util.List;
 
 public class ViewMoreReview {
 
@@ -17,5 +17,13 @@ public class ViewMoreReview {
 
         WebElement webElementReviewMore = driver.findElement(By.xpath("//div[@id='kakaoWrap']/div[@id='kakaoContent']/div[@id='mArticle']/div[@data-viewid = 'comment']/div[@class='evaluation_review']/a[@class='link_more']"));
         webElementReviewMore.sendKeys(Keys.ENTER);
+
+        Thread.sleep(3000);
+
+        List<WebElement> reviewsBefore = driver.findElements(By.xpath("//div[@class='comment_info']/p[@class='txt_comment']/span"));
+        List<WebElement> reviewsAfter = driver.findElements(By.xpath("//div[@class='comment_info']/p[@class='txt_comment txt_fold']/span"));
+
+        System.out.println("reviewsBefore.size() = " + reviewsBefore.size());
+        System.out.println("reviewsAfter.size() = " + reviewsAfter.size());
     }
 }
