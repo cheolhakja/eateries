a63966aac5a36659e080611bf92c719d9f1b4ab3
jeon-ji-yoon <rebegin15@gmail.com>
2023-01-05 15:24:01 +0900

    직렬화 실행

--- src/main/java/com/sobresalir/demo/ViewMoreReview.java
+++ src/main/java/com/sobresalir/demo/ViewMoreReview.java
@@ -23,7 +23,29 @@ public class ViewMoreReview {
         List<WebElement> reviewsBefore = driver.findElements(By.xpath("//div[@class='comment_info']/p[@class='txt_comment']/span"));
         List<WebElement> reviewsAfter = driver.findElements(By.xpath("//div[@class='comment_info']/p[@class='txt_comment txt_fold']/span"));
 
-        System.out.println("reviewsBefore.size() = " + reviewsBefore.size());
-        System.out.println("reviewsAfter.size() = " + reviewsAfter.size());
+        System.out.println("reviewsBefore.size() = " + reviewsBefore.size()); //리뷰들
+        System.out.println("reviewsAfter.size() = " + reviewsAfter.size()); //마지막 리뷰
+        Thread.sleep(1000);
+
+        //리뷰를 출력해본다
+        for (int j = 0; j < reviewsBefore.size(); j++) {
+            System.out.println(reviewsBefore.get(j).getText());
+            Thread.sleep(1000);
+            System.out.println();
+        }
+
+        Thread.sleep(1000);
+        for (int j = 0; j < reviewsAfter.size(); j++) {
+            System.out.println(reviewsAfter.get(j).getText());
+            Thread.sleep(1000);
+            System.out.println();
+        }
+
+        //주의: 수많은 .JSON파일을 생성할 수 있으므로 딱 한번만 실행하도록 한다.
+        //이미 있으면 덮어 쓰는게 어떻겠나?
+
+        Thread.sleep(1000);
+        JSONSerialization.serialize(reviewsBefore);
+
     }
 }
