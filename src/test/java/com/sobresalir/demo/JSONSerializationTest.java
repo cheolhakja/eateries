fb69cb250a900e610b7986537d1da574d9a99a7f
jeon-ji-yoon <rebegin15@gmail.com>
2023-01-05 15:23:04 +0900

    직렬화 테스트
    절대 경로에 있는 파일을 체크하는 Assertion을 넣으면 더 좋을듯

---
+++ src/test/java/com/sobresalir/demo/JSONSerializationTest.java
@@ -0,0 +1,11 @@
+package com.sobresalir.demo;
+
+import org.junit.jupiter.api.Test;
+
+public class JSONSerializationTest {
+
+    @Test
+    public void JSON_직렬화_테스트() throws InterruptedException {
+        JSONSerialization.serialize();
+    }
+}
