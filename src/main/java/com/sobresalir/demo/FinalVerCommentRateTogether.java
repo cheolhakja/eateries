81c2c140219e911620ce40094691b1d9af7cb6e1
cheolhakja <rebegin15@gmail.com>
2023-01-10 15:56:39 +0900

    add: 별점과 리뷰를 묶고 JSON으로 직렬화

--- src/main/java/com/sobresalir/demo/FinalVerCommentRateTogether.java
+++ src/main/java/com/sobresalir/demo/FinalVerCommentRateTogether.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FinalVerCommentRateTogether {
@@ -28,21 +29,23 @@ public class FinalVerCommentRateTogether {
         List<WebElement> ratingSorted =driver.findElements(By.xpath("//span[@class = 'ico_star star_rate']/span"));
         Thread.sleep(1000);
 
-        System.out.println("ratingSorted.size() = " + ratingSorted.size());
-        Thread.sleep(1000);
+        /*System.out.println("ratingSorted.size() = " + ratingSorted.size());
+        Thread.sleep(1000);*/
 
 
-        for (int i = 0; i < 10; i++) {
+        /*for (int i = 0; i < 10; i++) {
             System.out.println("elements" + i + " = " +  ratingSorted.get(i).getAttribute("style"));
             Thread.sleep(1000);
 
-        }
+        }*/
 
-        for (int i = 0 + 2; i < 10 - 1; i++) { //+2와 -1을 알아보기 쉽게 refactor하기
-            System.out.println("elements" + i + " = " +  ratingSorted.get(i).getAttribute("style"));
+        List<String> answerForRatings = new ArrayList<>(); //맨날 ArrayList만 쓰나
+        for (int i = 0 + 2; i < ratingSorted.size() - 1; i++) { //+2와 -1을 알아보기 쉽게 refactor하기
+
+            answerForRatings.add(ratingSorted.get(i).getAttribute("style"));
             Thread.sleep(1000);
 
-        }
+        } //진짜로 필요한 것
 
         //comment
 
@@ -50,10 +53,11 @@ public class FinalVerCommentRateTogether {
 
         Thread.sleep(1000);
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("리뷰 = " + elementTextReview.get(i).getText());
-            Thread.sleep(1000);
+        List<String> answerForReviews = new ArrayList<>();
+        for (int i = 0; i < answerForReviews.size(); i++) {
 
+            answerForReviews.add(elementTextReview.get(i).getText());
+            Thread.sleep(1000);
         }
 
         //문자열 출력
@@ -125,5 +129,53 @@ public class FinalVerCommentRateTogether {
         for (int i = 0; i < elementTextReview.size(); i++) {
 
         }*/
+    }
+
+    public static List<RatingReviewData> scrap() throws InterruptedException {
+        ChromeDriver driver = new ChromeDriver(); //크롬 브라우저를 연다
+        driver.get("https://place.map.kakao.com/10346328");
+        Thread.sleep(1000);
+
+        while(true) {
+            try {
+                WebElement elementReviewMore = driver.findElement(By.xpath("//div[@id='kakaoWrap']/div[@id='kakaoContent']/div[@id='mArticle']/div[@data-viewid = 'comment']/div[@class='evaluation_review']/a[@class='link_more']"));
+                elementReviewMore.sendKeys(Keys.ENTER);
+                Thread.sleep(1000);
+            } catch(org.openqa.selenium.NoSuchElementException e) {
+                System.out.println("후기를 전부 펼쳤습니다");
+                break;
+            }
+        }
+
+        List<WebElement> ratingSorted =driver.findElements(By.xpath("//span[@class = 'ico_star star_rate']/span"));
+        Thread.sleep(3000);
+        List<WebElement> elementTextReview = driver.findElements(By.xpath("//ul[@class = 'list_evaluation']/li"));
+        Thread.sleep(3000);
+
+        List<String> answerForRatings = new ArrayList<>(); //맨날 ArrayList만 쓰나
+        List<String> answerForReviews = new ArrayList<>();
+
+        for (int i = 0 + 2; i < ratingSorted.size() - 1; i++) { //+2와 -1을 알아보기 쉽게 refactor하기
+            String s1 = ratingSorted.get(i).getAttribute("style");
+            Thread.sleep(1000);
+
+            answerForRatings.add(s1);
+            //Thread.sleep(1000);
+        }
+
+        for (int i = 0; i < elementTextReview.size(); i++) {
+            String s2 = elementTextReview.get(i).getText();
+            Thread.sleep(1000);
+
+            answerForReviews.add(s2);
+            //Thread.sleep(1000);
+        }
+
+        List<RatingReviewData> answer = new ArrayList<>();
+        for (int i = 0; i < answerForReviews.size(); i++) {
+            answer.add(new RatingReviewData(answerForRatings.get(i), answerForReviews.get(i)));
+        }
+
+        return answer;
     }
 }
--- src/main/java/com/sobresalir/demo/Main.java
+++ src/main/java/com/sobresalir/demo/Main.java
@@ -1,26 +1,42 @@
 package com.sobresalir.demo;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","./chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver(); //크롬 브라우저를 연다
-        driver.get("https://map.kakao.com/");
-        Thread.sleep(1000);
 
-        WebElement webElementKeyWord = driver.findElement(By.id("search.keyword.query"));
-        webElementKeyWord.sendKeys("명동 순대국");
-        WebElement webElementSubmit = driver.findElement(By.id("search.keyword.submit"));
-        webElementSubmit.sendKeys(Keys.ENTER);
-        Thread.sleep(1000);
+        List<RatingReviewData> scrap = FinalVerCommentRateTogether.scrap();
 
-        WebElement webElementStoreMore = driver.findElement(By.id("info.search.place.more"));
-        webElementStoreMore.sendKeys(Keys.ENTER);
+        System.out.println("scrap.size() = " + scrap.size());
 
+        JSONObject obj = new JSONObject();
+        int i;
+
+        for (i = 0; i < scrap.size(); i++) {
+            JSONArray listJSON = new JSONArray();
+            String key1 = "rating" + (i + 1); //이펙티브 자바 문자열?
+            String key2 = "review" + (i + 1);
+            RatingReviewData ratingReviewData = scrap.get(i);
+            listJSON.add(ratingReviewData.getRating());
+            listJSON.add(ratingReviewData.getReview());
+            obj.put(key2, listJSON);
+
+        }
+        obj.put("number", (i + 1));
+        try {
+            String fileName = "output-final";
+            FileWriter file = new FileWriter("C:\\Users\\IBK\\Desktop\\나만의-우테코\\kakaomap-data-scrap\\reviewdata\\second-trial\\" + fileName);
+            file.write(obj.toJSONString());
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("JSON file created: "+obj);
     }
 }
