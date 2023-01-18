f98c311411325307ac06cacafb0f908a943157ff
cheolhakja <rebegin15@gmail.com>
2023-01-18 11:09:37 +0900

    add: 텍스트에서 접두어 제거

--- src/main/java/com/sobresalir/demo/ParseCommentFromTextData.java
+++ src/main/java/com/sobresalir/demo/ParseCommentFromTextData.java
@@ -20,6 +20,24 @@ public class ParseCommentFromTextData {
         return text.substring(startIndex, lastIndex - 10);
     }
 
+    public static String removePrefix(String text) {
+        int cnt = 0;
+        int startIndex = 0;
+        int lastIndex = text.length();
+        for (int i = 0; i < text.length(); i++) {
+            if(text.charAt(i) == '\n') {
+                cnt++;
+                if(cnt == 6) {
+                    startIndex = i;
+                    break;
+                }
+
+            }
+
+        }
+        return text.substring(startIndex, lastIndex - 1);
+    }
+
     public static String eliminateSuffixOfLikeNumber(String text) {
         int length = text.length();
         char c = text.charAt(length - 8); //8을 상수로 지정
