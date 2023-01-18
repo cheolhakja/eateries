5738c46367ab6c5ef69b65e82591404e46fe0b0c
cheolhakja <rebegin15@gmail.com>
2023-01-18 11:21:32 +0900

    add: 별점 - 리뷰 쌍을 저장하는 객체 추가

---
+++ src/main/java/com/sobresalir/demo/RatingReviewData.java
@@ -0,0 +1,37 @@
+package com.sobresalir.demo;
+
+class RatingReviewData {
+
+    //public 삭제
+    private String rating;
+    private String review;
+
+    public RatingReviewData(String rating, String review) {
+        this.rating = rating;
+        this.review = review;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    @Override
+    public String toString() {
+        return "RatingReviewData{" +
+                "rating='" + rating + '\'' +
+                ", review='" + review + '\'' +
+                '}';
+    }
+}
