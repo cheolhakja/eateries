53543427513cfa5787ef74082a82403c95daf73f
cheolhakja <rebegin15@gmail.com>
2023-01-19 11:15:27 +0900

    add: 가게명 크롤링 후 파일이름에 반영하기

--- src/main/java/com/sobresalir/demo/Main2.java
+++ src/main/java/com/sobresalir/demo/Main2.java
@@ -2,6 +2,9 @@ package com.sobresalir.demo;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,16 +14,18 @@ public class Main2 {
     public static void main(String[] args) throws InterruptedException {
         List<RatingReviewData> scrap = FinalVerCommentRateTogether.scrap();
 
+        String storeName = Main2.getStoreName();
+
         for (int i = 0; i < scrap.size(); i++) {
             String review = scrap.get(i).getReview();
             String result = ParseCommentFromTextData.doAll(review);
             scrap.get(i).setReview(result);
         }
 
-        Main2.serialize(scrap);
+        Main2.serialize(scrap, storeName);
     }
 
-    private static void serialize(List<RatingReviewData> list) {
+    private static void serialize(List<RatingReviewData> list, String storeName) {
         JSONObject obj = new JSONObject();
         int i;
 
@@ -36,7 +41,7 @@ public class Main2 {
         }
         obj.put("number", (i));
         try {
-            String fileName = "output-final";
+            String fileName = "output-final" + storeName;
             FileWriter file = new FileWriter("C:\\Users\\IBK\\Desktop\\나만의-우테코\\kakaomap-data-scrap\\reviewdata\\second-trial\\" + fileName);
             file.write(obj.toJSONString());
             file.close();
@@ -44,5 +49,14 @@ public class Main2 {
             e.printStackTrace();
         }
         System.out.println("JSON file created: "+obj);
+    }
+
+    public static String getStoreName() throws InterruptedException {
+        ChromeDriver driver = new ChromeDriver(); //크롬 브라우저를 연다
+        driver.get("https://place.map.kakao.com/10346328");
+        Thread.sleep(1000);
+
+        WebElement elementStoreName = driver.findElement(By.xpath("//div[@class = 'inner_place']/h2[@class = 'tit_location']"));
+        return elementStoreName.getText();
     }
 }
